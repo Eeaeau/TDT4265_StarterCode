6676301d25a4057660ac21374b7b95d16b72a21a
Sebastian <sebskogen@gmail.com>
2022-03-04 21:36:02 +0100

    Added assignment 3 handin

--- assignment3/create_submission_zip.py
+++ assignment3/create_submission_zip.py
@@ -22,7 +22,10 @@ def query(question, options):
 files_to_include = {
     "task2": [".py"],
     "task2_train": [".ipynb"],
+    "task3": [".py"],
+    "task4a": [".py"],
     "task4b": [".py"],
+    "task4c": [".py"],
     "trainer": [".py"],
     "utils": [".py"],
     "dataloaders": [".py"]
