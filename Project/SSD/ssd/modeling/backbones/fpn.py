e24dbc7e30b0ce850d59c37212ec4363e3168784
Sebastian <sebskogen@gmail.com>
2022-04-29 00:05:21 +0200

    corrent in_channel size

--- Project/SSD/ssd/modeling/backbones/fpn.py
+++ Project/SSD/ssd/modeling/backbones/fpn.py
@@ -1,3 +1,4 @@
+from queue import PriorityQueue
 import torch
 from torchvision.models import resnet101
 from torchvision.models.resnet import BasicBlock
@@ -11,10 +12,10 @@ from torch import nn
 
 # To assist you in designing the feature extractor you may want to print out
 # the available nodes for resnet50.
-m = resnet101(pretrained=True)
+# m = resnet101(pretrained=True)
 
-train_nodes, eval_nodes = get_graph_node_names(resnet101(pretrained=True))
-print(train_nodes,eval_nodes)
+# train_nodes, eval_nodes = get_graph_node_names(resnet101(pretrained=True))
+# print(train_nodes,eval_nodes)
 # The lists returned, are the names of all the graph nodes (in order of
 # execution) for the input model traced in train mode and in eval mode
 # respectively. You'll find that `train_nodes` and `eval_nodes` are the same
@@ -39,14 +40,14 @@ return_nodes = {
 # has multiple outputs. It's not always guaranteed that the last operation
 # performed is the one that corresponds to the output you desire. You should
 # consult the source code for the input model to confirm.)
-return_nodes = {
-    'layer1': 'layer1',
-    'layer2': 'layer2',
-    'layer3': 'layer3',
-    'layer4': 'layer4',
-    'layer5': 'layer5',
-    'layer6': 'layer6',
-}
+# return_nodes = {
+#     'layer1': 'layer1',
+#     'layer2': 'layer2',
+#     'layer3': 'layer3',
+#     'layer4': 'layer4',
+#     'layer5': 'layer5',
+#     'layer6': 'layer6',
+# }
 
 # Now you can build the feature extractor. This returns a module whose forward
 # method returns a dictionary like:
@@ -78,37 +79,37 @@ class Layer(nn.Sequential):
             nn.ReLU(),
         )
 
-
 # MaskRCNN requires a backbone with an attached FPN
 class Resnet101WithFPN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         # super(Resnet101WithFPN, self).__init__()
 
+        self.out_channels = [256, 256, 256, 2048, 2048, 2048]
         # Get a resnet50 backbone
         m = resnet101(pretrained=True)
 
+        # m.add_module('layer5', torch.nn.Sequential(
+        #         BasicBlock (inplanes = self.out_channels[-3], planes = self.out_channels[-2], stride = 2,
+        #         downsample = nn.Sequential(
+        #             nn.Conv2d(in_channels=self.out_channels[-3], out_channels=self.out_channels[-2], kernel_size=1, stride=2),
+        #             nn.ReLU()))
+        #     )
+        # )
 
+        # m.add_module('layer6', torch.nn.Sequential(torch.nn.Sequential(
+        #         BasicBlock (inplanes = self.out_channels[-2], planes = self.out_channels[-1], stride = 2,
+        #         downsample = nn.Sequential(
+        #             nn.Conv2d(in_channels=self.out_channels[-2], out_channels=self.out_channels[-1], kernel_size=1, stride=2),
+        #             nn.ReLU()))
+        #         )
+        #     )
+        # )
+        # print(m)
 
-        # Extract 4 main layers (note: MaskRCNN needs this particular name
-        # mapping for return nodes)
-        self.body = create_feature_extractor(
-            m, return_nodes={f'layer{k}': str(v)
-                             for v, k in enumerate([1, 2, 3, 4])})
-
-        # Dry run to get number of channels for FPN
-        inp = torch.randn(1, 3, 128, 1024)
-        with torch.no_grad():
-            out = self.body(inp)
-        in_channels_list = [o.shape[1] for o in out.values()]
-        print(in_channels_list)
-        # Build FPN
-        self.out_channels = [256]*6
-        # self.out_channels = [256, 512, 1024, 2048]
-        self.fpn = FeaturePyramidNetwork(
-            in_channels_list, out_channels=256)
-
-
+        # train_nodes, eval_nodes = get_graph_node_names(m)
+        # print("###############################################:", train_nodes, eval_nodes)
+        # print(get_graph_node_names(m))
         self.extras = nn.ModuleList([
             torch.nn.Sequential(
                 BasicBlock (inplanes = self.out_channels[-3], planes = self.out_channels[-2], stride = 2,
@@ -124,6 +125,62 @@ class Resnet101WithFPN(torch.nn.Module):
                     nn.ReLU())),
                 )
         ])
+
+
+
+        # Extract 4 main layers (note: MaskRCNN needs this particular name
+        # mapping for return nodes)
+        # self.body = create_feature_extractor(
+        #     m, return_nodes=return_nodes)
+        self.body = create_feature_extractor(
+            m, return_nodes={f'layer{k}': str(v)
+                             for v, k in enumerate([1, 2, 3, 4])})
+
+        # self.body.add_module('layer5', torch.nn.Sequential(
+        #         BasicBlock (inplanes = self.out_channels[-3], planes = self.out_channels[-2], stride = 2,
+        #         downsample = nn.Sequential(
+        #             nn.Conv2d(in_channels=self.out_channels[-3], out_channels=self.out_channels[-2], kernel_size=1, stride=2),
+        #             nn.ReLU()))
+        #     )
+        # )
+
+        # self.body.add_module('layer6', torch.nn.Sequential(torch.nn.Sequential(
+        #         BasicBlock (inplanes = self.out_channels[-2], planes = self.out_channels[-1], stride = 2,
+        #         downsample = nn.Sequential(
+        #             nn.Conv2d(in_channels=self.out_channels[-2], out_channels=self.out_channels[-1], kernel_size=1, stride=2),
+        #             nn.ReLU()))
+        #         )
+        #     )
+        # )
+
+        # print(self.body)
+        # Dry run to get number of channels for FPN
+        inp = torch.randn(1, 3, 128, 1024)
+
+        with torch.no_grad():
+            out = self.body(inp)
+            # print(out)
+            x = out["3"]
+
+            in_channels_list = [o.shape[1] for o in out.values()]
+
+        # print(self.extras[0](x))
+
+        with torch.no_grad():
+            for i, extra in enumerate(self.extras):
+                print(i)
+                x = extra(x)
+                in_channels_list.append(x.shape[1])
+
+
+        print("---------------------------------------", in_channels_list, "---------------------------------------")
+
+        # Build FPN
+        # self.out_channels = [256, 512, 1024, 2048]
+        self.fpn = FeaturePyramidNetwork(
+            in_channels_list, out_channels=256)
+
+
         print("############################################################")
     def forward(self, x):
 
@@ -132,7 +189,9 @@ class Resnet101WithFPN(torch.nn.Module):
 
         for extra in self.extras:
             x = extra(x)
+            print(x)
             features.append(x)
+
 
         x = self.fpn(x)
 
