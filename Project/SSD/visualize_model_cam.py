47a9ede8889a36dc92bae1ad42b1edcae06ee6d5
Sebastian <sebskogen@gmail.com>
2022-05-05 16:29:31 +0200

    Add as python file

---
+++ Project/SSD/visualize_model_cam.py
@@ -0,0 +1,15 @@
+from torchvision.io.image import read_image
+import matplotlib.pyplot as plt
+import torch.functional as F
+from torchvision.transforms.functional import normalize, resize, to_pil_image
+
+
+# from ssd.modeling.retinanetOutputWrapper import RetinaNetOutputWrapper
+
+config_path = "configs/tdt4265_fpn.py"
+# cfg = LazyConfig.load(config_path)
+
+# Get your input
+img = read_image("data/tdt4265_2022/images/train/trip007_glos_Video00000_3.png")
+plt.imshow(to_pil_image(img))
+plt.show()
