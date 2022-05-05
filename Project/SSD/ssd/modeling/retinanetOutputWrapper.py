f9532068ee1f6c465409c1b5c2b02a2820fd81b0
Sebastian <sebskogen@gmail.com>
2022-05-05 15:33:20 +0200

    doing some more testing

--- Project/SSD/ssd/modeling/retinanetOutputWrapper.py
+++ Project/SSD/ssd/modeling/retinanetOutputWrapper.py
@@ -8,10 +8,9 @@ class RetinaNetOutputWrapper(torch.nn.Module):
     def __init__(self, model):
         super().__init__()
         self.model = model
-        print(self.model)
-        print("#############################################################")
+        # print(self.model)
+        # print("#############################################################")
 
     def forward(self, x):
-
-        return self.model(x)
+        return self.model(x)[0][0]
         # exit()
