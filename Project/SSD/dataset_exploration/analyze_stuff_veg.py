c85adabe360c187c07814c4e9b3aafd1db357065
Sebastian <sebskogen@gmail.com>
2022-05-08 09:59:08 +0200

    height and width spread

--- Project/SSD/dataset_exploration/analyze_stuff_veg.py
+++ Project/SSD/dataset_exploration/analyze_stuff_veg.py
@@ -402,6 +402,21 @@ def heightPlot_hue(df, hue, show_fig=False):
         plt.savefig('./dataset_exploration/height_px_perclass_kde.eps', dpi=200)
         plt.show()
 
+def heightBoxPlot(df, hue='dataset', show_fig=False):
+
+    g = sns.boxplot(x=df['height']*128, y="type", data=df, hue=hue, showfliers=False)
+
+    g.set(xlabel='Pixel height')
+
+    plt.title('Spread of anchor box height')
+    plt.tight_layout()
+    if show_fig:
+        plt.show()
+    else:
+        plt.savefig('./dataset_exploration/height_px_perclass_spread.png', dpi=200)
+        plt.savefig('./dataset_exploration/height_px_perclass_spread.eps', dpi=200)
+        plt.savefig('./dataset_exploration/height_px_perclass_spread.svg', dpi=200)
+
 def widthPlot_hue(df, hue, show_fig=False):
 
     #g = sns.displot(x=df['height']/df['width'],data=df, hue=hue,col='dataset',kde=True)
@@ -419,6 +434,23 @@ def widthPlot_hue(df, hue, show_fig=False):
         plt.savefig('./dataset_exploration/width_px_perclass_kde.png', dpi=200)
         plt.savefig('./dataset_exploration/width_px_perclass_kde.eps', dpi=200)
         plt.show()
+
+
+def widthBoxPlot(df, hue='dataset', show_fig=False):
+
+    g = sns.boxplot(x=df['width']*1024, y="type", data=df, hue=hue, showfliers=False)
+
+    g.set(xlabel='Pixel width')
+
+    plt.title('Spread of anchor box width')
+    plt.tight_layout()
+    if show_fig:
+        plt.show()
+    else:
+        plt.savefig('./dataset_exploration/width_px_perclass_spread.png', dpi=200)
+        plt.savefig('./dataset_exploration/width_px_perclass_spread.eps', dpi=200)
+        plt.savefig('./dataset_exploration/width_px_perclass_spread.svg', dpi=200)
+
 
 def plotOverlapPerFrame(dataloader_train, dataloader_val, cfg, show_fig=False):
     t, v = numOverlapped(dataloader_train, dataloader_val,cfg)
@@ -463,7 +495,7 @@ def main():
     #plotOverlapPerFrame(dataloader_train,dataloader_val,cfg)
 
     df = createDataframe(dataloader_train, dataloader_val, labels)
-    aspect_ratio_box(df, show_fig=False)
+    heightBoxPlot(df, show_fig=False)
     #plotCenters(df)
     #aspectRatioPlot(df)
     #print(df.head())
